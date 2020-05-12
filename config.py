1b9ce8642007052aeca677725a9e45ae4fcc6f53
glasses-png <cchelangat16@gmail.com>
2020-05-12 08:09:59 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -1,15 +1,28 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
+
 class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://charlotte:2004@localhost/blog'
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+  
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE = os.environ.get("DATABASE_URL")
+    pass 
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://charlotte:2004@localhost/blog' 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
