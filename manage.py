142e4de702452bd0a9d43afa7a2974dfdd5ad655
glasses-png <cchelangat16@gmail.com>
2020-05-12 13:31:00 +0300

    changes

--- manage.py
+++ manage.py
@@ -15,14 +15,14 @@ manager.add_command('server', Server)
 def make_shell_context():
     return dict(app = app,db = db,User = User)
 
-# @manager.command
-# def test():
-#     '''
-#     Run the unit tests
-#     '''
-#     import unittest
-#     tests = unittest.TestLoader().discover('tests')
-#     unittest.TextTestRunner(verbosity=5).run(tests)
+@manager.command
+def test():
+    '''
+    Run the unit tests
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=5).run(tests)
 
 if __name__ == '__main__':
     manager.run()
